bb24c76442035e5b71797fdc55200d9dd8b01c43
Ian Bui <thinkbui@gmail.com>
2023-05-02 01:06:45 -0500

    Added API post

--- routes/notes.js
+++ routes/notes.js
@@ -8,4 +8,41 @@ notes.get('/', (req, res) => {
   );
 });
 
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text
+    };
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
 module.exports = notes;
